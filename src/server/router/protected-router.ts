254a97c8e0e10d52dda5277b9cc0f0731fdbc0f8
klmkyo <mail@mklimek.dev>
2022-08-01 11:20:27 +0200

    nice formatting

--- src/server/router/protected-router.ts
+++ src/server/router/protected-router.ts
@@ -5,7 +5,8 @@ import { createRouter } from "./context";
  * Creates a tRPC router that asserts all queries and mutations are from an authorized user. Will throw an unauthorized error if a user is not signed in.
  */
 export function createProtectedRouter() {
-  return createRouter().middleware(({ ctx, next }) => {
+  return createRouter()
+    .middleware(({ ctx, next }) => {
     if (!ctx.session || !ctx.session.user) {
       throw new trpc.TRPCError({ code: "UNAUTHORIZED" });
     }
