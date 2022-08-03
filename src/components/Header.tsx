240eb1225cb82865872caf6d0029fbfe457038a6
klmkyo <mail@mklimek.dev>
2022-08-03 20:01:08 +0200

    still ugly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,35 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Link from "next/link"
 
 export default function Header() {
 
   const { data: session } = useSession()
   return (
-    <div className="sticky top-0">
+    <div className="flex justify-between sticky top-0 p-4">
       {
         session ?
           <>
-            Signed in as {session.user!.email} <br />
+            Signed in as {session.user!.email}
+          </>
+          :
+          <>
+            Not signed in
+          </>
+      }
+
+      <div className="absolute left-1/2 -translate-x-1/2">
+        <Link href={"/"}>
+          Strona Główna
+        </Link>
+      </div>
+
+      {
+        session ?
+          <>
             <button onClick={() => signOut()}>Sign out</button>
           </>
           :
-          <>Not signed in <br />
+          <>
             <button onClick={() => signIn()}>Sign in</button>
           </>
       }
