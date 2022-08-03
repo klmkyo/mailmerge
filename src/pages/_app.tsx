497e00b60156e369bfc74d939b1b90044ea76251
klmkyo <mail@mklimek.dev>
2022-08-03 19:41:20 +0200

    damn vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,7 @@ export const getBaseUrl = () => {
   if (typeof window !== "undefined") {
     return "";
   }
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.VERCEL_URL) return `https://mailmerge.vercel.app`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
