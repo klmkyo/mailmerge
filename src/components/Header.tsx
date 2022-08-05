d16331b574a73efef1e6873b9efae4d4bf3c25d9
klmkyo <mail@mklimek.dev>
2022-08-05 02:02:17 +0200

    noice

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function Header() {
 
+  // get current page
+  const { pathname } = useRouter()
+
   const { data: session } = useSession()
   return (
-    <div className="flex justify-between sticky top-0 p-4 bg-stone-200 z-10">
+    <div className="flex justify-between items-center sticky top-0 p-4 bg-stone-200 z-10">
       {
         session ?
           <>
@@ -17,11 +21,17 @@ export default function Header() {
           </>
       }
 
-      <div className="absolute left-1/2 -translate-x-1/2">
-        <Link href={"/"}>
-          Strona Główna
-        </Link>
-      </div>
+      {
+        pathname === "/" || 
+        <div className="absolute left-1/2 -translate-x-1/2">
+          <Link href={"/"}>
+            <a className="text-2xl leading-normal font-extrabold text-gray-700 drop-shadow-xl">
+              Mail<span className="text-purple-400">Merge</span>
+            </a>
+          </Link>
+        </div>
+      }
+      
 
       {
         session ?
