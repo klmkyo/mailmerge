7dde4a58b037b3892b5f27a7eac5ee4c9417a299
klmkyo <mail@mklimek.dev>
2022-08-20 18:32:22 +0200

    thank god

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    screens: {
-      'sm': {max: '1599px'},
-      // => @media (min-width: 1599px) { ... }
+    extend: {
+      screens: {
+        'sm': {max: '1599px'},
+        // => @media (min-width: 1599px) { ... }
+      },
     },
-
   },
   plugins: [],
 };
